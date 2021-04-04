5d9c51011bb769666034c655e8fff5859dc6fde0
Elias Little <eliasjlittle@gmail.com>
2021-04-03 23:07:39 -0400

    Cleanup

--- RandomResearch/ScratchNN/Sample/accuracy_check.jl
+++
@@ -1,7 +0,0 @@
-"""
-    Check the accuracy between predicted values (Ŷ) and the true values(Y).
-"""
-function assess_accuracy(Ŷ , Y)
-    @assert size(Ŷ) == size(Y)
-    return sum((Ŷ .> 0.5) .== Y) / length(Y)
-end
